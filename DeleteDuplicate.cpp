dddb74547e34c67a2e307dbdd15742addecee372
Li <xinli11999@yahoo.com>
2013-03-04 10:01:23 -0800

    working now
    need to add more test cases

--- DeleteDuplicate.cpp
+++ DeleteDuplicate.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <iostream>
 
+
+
 struct ListNode 
 {
 	int val;
@@ -9,16 +11,50 @@ struct ListNode
     //ListNode(int x) : val(x), next(NULL) {}
  };
 
-void main()
+
+
+void printLinkedList(ListNode* start)
 {
-	std::cout<< "hello world";
+	ListNode* point = start;
+
+	while(point!= NULL)
+	{
+		std::cout << point->val << std::endl;
+		point = point->next;
+	}
+}
+
+ListNode* createLinkedList()
+{
+	// create a linkedList   2,1,1,4
+	const int count = 4;
+	int valueArray[count] = {2,1,1,4};
+
+	ListNode* a = new ListNode();
+	a->val = valueArray[0];
+
+	ListNode* b = new ListNode();
+	b->val = valueArray[1];
+
+	ListNode* c = new ListNode();
+	c->val = valueArray[2];
+
+	ListNode* d = new ListNode();
+	d->val = valueArray[3];
+
+	a->next = b;
+	b->next = c;
+	c->next = d;
+	d->next = NULL;
+
+	return a;
 }
 
 
 class Solution {
 
 public:
-    ListNode *deleteDuplicates(ListNode *head) 
+    static ListNode *deleteDuplicates(ListNode *head) 
     {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
@@ -45,3 +81,12 @@ public:
         return head;        
     }
 };
+
+void main()
+{
+	//std::cout<< "hello world";
+	ListNode* start = createLinkedList();
+	ListNode* result = Solution::deleteDuplicates(start);
+	printLinkedList(result);
+	std::cin;
+}
